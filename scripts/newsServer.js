8d1970715c0e1a18d18041deaa08240038d5ac7e
nyoungstudios <nathanielyoung95070@gmail.com>
2016-11-12 14:28:35 -0500

    server variables to grab data from json

--- scripts/newsServer.js
+++ scripts/newsServer.js
@@ -4,8 +4,19 @@ var cityName = "San Francisco";
 
 
 $.get("https://newsapi.org/v1/articles?source=" + source + "&sortBy=" + sortBy +"&apiKey=eba604d3c61e4c06a49c9c2d727b7659", function(data, status){
-    console.log(data);  //just for visualization
-    console.log(data['articles']['0']['author']);
+    //console.log(data);  //just for visualization
+    
+    var author; var description; var publishedAt; var title; var url; var urlToImage;
+    
+    for (var i = 0; i < data['articles'].length; i++) {
+        author = data['articles'][i]['author'];
+        description = data['articles'][i]['description'];
+        publishedAt = data['articles'][i]['publishedAt'];
+        title = data['articles'][i]['title'];
+        url = data['articles'][i]['url'];
+        urlToImage = data['articles'][i]['urlToImage'];
+    }
+    
     
     
 });
@@ -13,5 +24,14 @@ $.get("https://newsapi.org/v1/articles?source=" + source + "&sortBy=" + sortBy +
 
 $.get("http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&APPID=a29fff7fa1bce3c0229d7f6f97530e87", function(data, status){
     console.log(data);  //just for visualization
-    console.log(data['main']['temp']);
+    
+    var name = data['name'];
+    var humidity = data['main']['humidity'];
+    var temp = data['main']['temp'];
+    var temp_max = data['main']['temp_max'];
+    var temp_min = data['main']['temp_min'];
+    var windDeg = data['wind']['deg'];
+    var windSpeed = data['wind']['speed'];
+    var description = data['weather']['0']['description'];
+    
 });
