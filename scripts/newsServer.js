647b5a5c92747c235c6041100639f43083377420
nyoungstudios <nathanielyoung95070@gmail.com>
2016-11-12 09:03:17 -0500

    can change the wevsite for news

--- scripts/newsServer.js
+++ scripts/newsServer.js
@@ -1,12 +1,15 @@
+var source = "techcrunch";  //"the-next-web" is default
 var sortBy = "latest";  //top, latest, popular
-var cityName = "Nashville";
+var cityName = "San Francisco";
 
 
-$.get("https://newsapi.org/v1/articles?source=the-next-web&sortBy=" + sortBy +"&apiKey=eba604d3c61e4c06a49c9c2d727b7659", function(data, status){
-        console.log(data);
-    });
+$.get("https://newsapi.org/v1/articles?source=" + source + "&sortBy=" + sortBy +"&apiKey=eba604d3c61e4c06a49c9c2d727b7659", function(data, status){
+    console.log(data);
+    //var articles = $.parseJSON(data);
+    //console.log(articles.name === "John")
+});
 
 
 $.get("http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&APPID=a29fff7fa1bce3c0229d7f6f97530e87", function(data, status){
-        console.log(data);
-    });
+    console.log(data);
+});
