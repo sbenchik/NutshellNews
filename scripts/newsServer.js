630e3aa41438d4443c4124554097524a046e996c
nyoungstudios <nathanielyoung95070@gmail.com>
2016-11-12 11:29:51 -0500

    parsing json works

--- scripts/newsServer.js
+++ scripts/newsServer.js
@@ -4,12 +4,20 @@ var cityName = "San Francisco";
 
 
 $.get("https://newsapi.org/v1/articles?source=" + source + "&sortBy=" + sortBy +"&apiKey=eba604d3c61e4c06a49c9c2d727b7659", function(data, status){
-    console.log(data);
-    //var articles = $.parseJSON(data);
-    //console.log(articles.name === "John")
+    console.log(data);  //just for visualization
+    console.log(data['articles']['0']['author']);
+    
+    
 });
 
 
-$.get("http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&APPID=a29fff7fa1bce3c0229d7f6f97530e87", function(data, status){
-    console.log(data);
+$.get("http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&APPID=a29fff7fa1bce3c0229d7f6f97530e87", function(data, status){
+    console.log(data);  //just for visualization
+    console.log(data[''])
 });
+
+/**
+var jsontext   = '{"name":"x","age":"11"}';
+var getContact = JSON.parse(jsontext);
+console.log(getContact.name);
+**/
