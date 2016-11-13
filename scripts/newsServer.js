c14b3ff0a692d154ee75e24bce66aeba555fed87
nyoungstudios <nathanielyoung95070@gmail.com>
2016-11-13 02:55:12 -0500

    more js stuff and ui improvements

--- scripts/newsServer.js
+++ scripts/newsServer.js
@@ -2,11 +2,11 @@ $(document).ready(function() {
 
     var source = "techcrunch";  //"the-next-web" is default
     var sortBy = "latest";  //top, latest, popular
-    var cityName = "Nashville";
-    
+    var cityName = "Nashville";    
+
 
     $.get("https://newsapi.org/v1/articles?source=" + source + "&sortBy=" + sortBy +"&apiKey=eba604d3c61e4c06a49c9c2d727b7659", function(data, status){
-        //console.log(data);  //just for visualization
+        console.log(data);  //just for visualization
 
         var author; var description; var publishedAt; var title; var url; var urlToImage;
 
@@ -17,21 +17,15 @@ $(document).ready(function() {
             title = data['articles'][i]['title'];
             url = data['articles'][i]['url'];
             urlToImage = data['articles'][i]['urlToImage'];            
-                                    
-            
-            $("#title" + i).text(title);
+
+
+            $("#title" + i).text(author + " - " + title);
             $("#newsImage" + i).attr("src", urlToImage);
             $("#description" + i).text(description);
             $("#description" + i).attr("href", url);
 
-            
-            
-            
-            
-            
-
         }
-        
+
 
     });
 
@@ -53,4 +47,256 @@ $(document).ready(function() {
 
     });
     
+    
+    $("#theNextWeb").click(function() {
+        //reseting the button colors
+        $("#theNextWeb").css('color', '#49BF59');
+        $("#BBC").css('color', 'white');
+        $("#CNN").css('color', 'white');
+        $("#WIRED").css('color', 'white');
+        $("#Time").css('color', 'white');
+        $("#IGN").css('color', 'white');
+        $("#techCrunch").css('color', 'white');
+        
+        source = "the-next-web";
+        $.get("https://newsapi.org/v1/articles?source=" + source + "&sortBy=" + sortBy +"&apiKey=eba604d3c61e4c06a49c9c2d727b7659", function(data, status){
+            //console.log(data);  //just for visualization
+
+            var author; var description; var publishedAt; var title; var url; var urlToImage;
+
+            for (var i = 0; i < data['articles'].length; i++) {
+                author = data['articles'][i]['author'];
+                description = data['articles'][i]['description'].substring(0, 100) + "...";
+                publishedAt = data['articles'][i]['publishedAt'];
+                title = data['articles'][i]['title'];
+                url = data['articles'][i]['url'];
+                urlToImage = data['articles'][i]['urlToImage'];            
+
+
+                $("#title" + i).text(author + " - " + title);
+                $("#newsImage" + i).attr("src", urlToImage);
+                $("#description" + i).text(description);
+                $("#description" + i).attr("href", url);
+
+            }
+
+
+        });
+    });
+    
+    $("#BBC").click(function() {
+        //reseting the button colors
+        $("#theNextWeb").css('color', 'white');
+        $("#BBC").css('color', '#49BF59');
+        $("#CNN").css('color', 'white');
+        $("#WIRED").css('color', 'white');
+        $("#Time").css('color', 'white');
+        $("#IGN").css('color', 'white');
+        $("#techCrunch").css('color', 'white');
+        
+        source = "bbc-news";
+        $.get("https://newsapi.org/v1/articles?source=" + source + "&sortBy=" + sortBy +"&apiKey=eba604d3c61e4c06a49c9c2d727b7659", function(data, status){
+            //console.log(data);  //just for visualization
+
+            var author; var description; var publishedAt; var title; var url; var urlToImage;
+
+            for (var i = 0; i < data['articles'].length; i++) {
+                author = data['articles'][i]['author'];
+                description = data['articles'][i]['description'].substring(0, 100) + "...";
+                publishedAt = data['articles'][i]['publishedAt'];
+                title = data['articles'][i]['title'];
+                url = data['articles'][i]['url'];
+                urlToImage = data['articles'][i]['urlToImage'];            
+
+
+                $("#title" + i).text(author + " - " + title);
+                $("#newsImage" + i).attr("src", urlToImage);
+                $("#description" + i).text(description);
+                $("#description" + i).attr("href", url);
+
+            }
+
+
+        });
+    });
+    
+    $("#CNN").click(function() {
+        //reseting the button colors
+        $("#theNextWeb").css('color', 'white');
+        $("#BBC").css('color', 'white');
+        $("#CNN").css('color', '#49BF59');
+        $("#WIRED").css('color', 'white');
+        $("#Time").css('color', 'white');
+        $("#IGN").css('color', 'white');
+        $("#techCrunch").css('color', 'white');;
+        
+        source = "cnn";
+        $.get("https://newsapi.org/v1/articles?source=" + source + "&sortBy=" + sortBy +"&apiKey=eba604d3c61e4c06a49c9c2d727b7659", function(data, status){
+            //console.log(data);  //just for visualization
+
+            var author; var description; var publishedAt; var title; var url; var urlToImage;
+
+            for (var i = 0; i < data['articles'].length; i++) {
+                author = data['articles'][i]['author'];
+                description = data['articles'][i]['description'].substring(0, 100) + "...";
+                publishedAt = data['articles'][i]['publishedAt'];
+                title = data['articles'][i]['title'];
+                url = data['articles'][i]['url'];
+                urlToImage = data['articles'][i]['urlToImage'];            
+
+
+                $("#title" + i).text(author + " - " + title);
+                $("#newsImage" + i).attr("src", urlToImage);
+                $("#description" + i).text(description);
+                $("#description" + i).attr("href", url);
+
+            }
+
+
+        });
+    });
+    
+    $("#WIRED").click(function() {
+        //reseting the button colors
+        $("#theNextWeb").css('color', 'white');
+        $("#BBC").css('color', 'white');
+        $("#CNN").css('color', 'white');
+        $("#WIRED").css('color', '#49BF59');
+        $("#Time").css('color', 'white');
+        $("#IGN").css('color', 'white');
+        $("#techCrunch").css('color', 'white');
+        
+        source = "wired-de";
+        $.get("https://newsapi.org/v1/articles?source=" + source + "&sortBy=" + sortBy +"&apiKey=eba604d3c61e4c06a49c9c2d727b7659", function(data, status){
+            //console.log(data);  //just for visualization
+
+            var author; var description; var publishedAt; var title; var url; var urlToImage;
+
+            for (var i = 0; i < data['articles'].length; i++) {
+                author = data['articles'][i]['author'];
+                description = data['articles'][i]['description'].substring(0, 100) + "...";
+                publishedAt = data['articles'][i]['publishedAt'];
+                title = data['articles'][i]['title'];
+                url = data['articles'][i]['url'];
+                urlToImage = data['articles'][i]['urlToImage'];            
+
+
+                $("#title" + i).text(author + " - " + title);
+                $("#newsImage" + i).attr("src", urlToImage);
+                $("#description" + i).text(description);
+                $("#description" + i).attr("href", url);
+
+            }
+
+
+        });
+    });
+    
+    $("#Time").click(function() {
+        //reseting the button colors
+        $("#theNextWeb").css('color', 'white');
+        $("#BBC").css('color', 'white');
+        $("#CNN").css('color', 'white');
+        $("#WIRED").css('color', 'white');
+        $("#Time").css('color', '#49BF59');
+        $("#IGN").css('color', 'white');
+        $("#techCrunch").css('color', 'white');
+        
+        source = "time";
+        $.get("https://newsapi.org/v1/articles?source=" + source + "&sortBy=" + sortBy +"&apiKey=eba604d3c61e4c06a49c9c2d727b7659", function(data, status){
+            //console.log(data);  //just for visualization
+
+            var author; var description; var publishedAt; var title; var url; var urlToImage;
+
+            for (var i = 0; i < data['articles'].length; i++) {
+                author = data['articles'][i]['author'];
+                description = data['articles'][i]['description'].substring(0, 100) + "...";
+                publishedAt = data['articles'][i]['publishedAt'];
+                title = data['articles'][i]['title'];
+                url = data['articles'][i]['url'];
+                urlToImage = data['articles'][i]['urlToImage'];            
+
+
+                $("#title" + i).text(author + " - " + title);
+                $("#newsImage" + i).attr("src", urlToImage);
+                $("#description" + i).text(description);
+                $("#description" + i).attr("href", url);
+
+            }
+
+
+        });
+    });
+    
+    $("#IGN").click(function() {
+        //reseting the button colors
+        $("#theNextWeb").css('color', 'white');
+        $("#BBC").css('color', 'white');
+        $("#CNN").css('color', 'white');
+        $("#WIRED").css('color', 'white');
+        $("#Time").css('color', 'white');
+        $("#IGN").css('color', '#49BF59');
+        $("#techCrunch").css('color', 'white');
+        
+        source ="ign";
+        $.get("https://newsapi.org/v1/articles?source=" + source + "&sortBy=" + sortBy +"&apiKey=eba604d3c61e4c06a49c9c2d727b7659", function(data, status){
+            //console.log(data);  //just for visualization
+
+            var author; var description; var publishedAt; var title; var url; var urlToImage;
+
+            for (var i = 0; i < data['articles'].length; i++) {
+                author = data['articles'][i]['author'];
+                description = data['articles'][i]['description'].substring(0, 100) + "...";
+                publishedAt = data['articles'][i]['publishedAt'];
+                title = data['articles'][i]['title'];
+                url = data['articles'][i]['url'];
+                urlToImage = data['articles'][i]['urlToImage'];            
+
+
+                $("#title" + i).text(author + " - " + title);
+                $("#newsImage" + i).attr("src", urlToImage);
+                $("#description" + i).text(description);
+                $("#description" + i).attr("href", url);
+
+            }
+
+
+        });
+    });
+    
+    $("#techCrunch").click(function() {
+        //reseting the button colors
+        $("#theNextWeb").css('color', 'white');
+        $("#BBC").css('color', 'white');
+        $("#CNN").css('color', 'white');
+        $("#WIRED").css('color', 'white');
+        $("#Time").css('color', 'white');
+        $("#IGN").css('color', 'white');
+        $("#techCrunch").css('color', '#49BF59');
+        
+        source = "techcrunch";
+        $.get("https://newsapi.org/v1/articles?source=" + source + "&sortBy=" + sortBy +"&apiKey=eba604d3c61e4c06a49c9c2d727b7659", function(data, status){
+            //console.log(data);  //just for visualization
+
+            var author; var description; var publishedAt; var title; var url; var urlToImage;
+
+            for (var i = 0; i < data['articles'].length; i++) {
+                author = data['articles'][i]['author'];
+                description = data['articles'][i]['description'].substring(0, 100) + "...";
+                publishedAt = data['articles'][i]['publishedAt'];
+                title = data['articles'][i]['title'];
+                url = data['articles'][i]['url'];
+                urlToImage = data['articles'][i]['urlToImage'];            
+
+
+                $("#title" + i).text(author + " - " + title);
+                $("#newsImage" + i).attr("src", urlToImage);
+                $("#description" + i).text(description);
+                $("#description" + i).attr("href", url);
+
+            }
+
+
+        });
+    });
 });
